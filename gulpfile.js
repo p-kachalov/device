281d7375f81f6906995a2b7bc8810d8aa702a6f1
Pavel K <pa.kachalov@gmail.com>
2018-01-11 15:32:30 +0300

    uglify by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
 var gulp = require("gulp");
 var rename = require("gulp-rename");
 var cleanCSS = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
 
 gulp.task("minify-css", function () {
   return gulp.src("./css/style.css")
     .pipe(cleanCSS())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("./css"));
+});
+
+gulp.task("uglify", function () {
+  return gulp.src("./js/script.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("./js"));
 });
