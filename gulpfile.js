cc4fb9f3145b8b72a916ac9cf9bbe7a44c621f76
Pavel K <pa.kachalov@gmail.com>
2018-01-11 15:46:15 +0300

    add gulp lint and defalut task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require("gulp");
 var rename = require("gulp-rename");
 var cleanCSS = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
+var eslint = require("gulp-eslint");
 
 gulp.task("minify-css", function () {
   return gulp.src("./css/style.css")
@@ -16,4 +17,15 @@ gulp.task("uglify", function () {
     .pipe(uglify())
     .pipe(rename("script.min.js"))
     .pipe(gulp.dest("./js"));
+});
+
+gulp.task("lint", function() {
+  return gulp.src(["**/*.js","!node_modules/**", "!**/*.min.js"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task("default", ["lint"], function () {
+  gulp.start("minify-css", "uglify");
 });
